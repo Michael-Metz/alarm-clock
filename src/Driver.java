31da962b1a50733ac00fa9a43d06f211f8a52c05
Espinosa, Adam <amespi1@ilstu.edu>
2017-11-13 19:46:55 -0600

    test

--- src/Driver.java
+++ src/Driver.java
@@ -2,5 +2,6 @@ class Driver{
     public static void main(String[] args){
         Model model = Model.getInstance();
         model.readXml();
+        System.out.println("Dean likes smelly feet.");
     }
 }
